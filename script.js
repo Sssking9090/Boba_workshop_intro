ff77f2329460c4fc3ab1f1fec117f30ee16feda8
Sssking9090 <120855836+Sssking9090@users.noreply.github.com>
2025-09-19 22:45:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function replaceBodyWithSadFace() {
+  const sadFaceArt = `
+       (╯︵╰,)
+  `;
+
+  // Replace body with a huge sad face
+  document.body.innerHTML = `<div>${sadFaceArt}</div>`;
+  document.body.innerHTML = `<div>${sadFaceArt}</div>`;
+  document.body.innerHTML = `<div>${sadFaceArt}</div>`;    
+}
